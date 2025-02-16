61704b4a1ba507fff22a80850e8fc29c8e810b4a
Vermaas <vermaas@astron.nl>
2025-02-16 12:19:24 +0100

    add filter buttons

--- ancients/settings/docker.py
+++ ancients/settings/docker.py
@@ -5,6 +5,8 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '/shared/ancients.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, '/shared/ancients.sqlite3'),
+        # it is not necessary to use an externally hosted database, the onboard sqlite file suffices
+        'NAME': os.path.join(BASE_DIR, 'ancients.sqlite3'),
     }
 }
--- places/views.py
+++ places/views.py
@@ -2,55 +2,19 @@ from .models import Place
 
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import ListView, TemplateView
-
 from places.services import algorithms
+
+
 
 def redirect_with_params(view_name, params):
     return redirect(reverse(view_name) + params)
 
-class IndexView(TemplateView):
-    model = Place
-    template_name = "index.html"
-    queryset = Place.objects.all()
-
-    def get_context_data(self, **kwargs):
-
-        context = (
-            super().get_context_data(
-                **kwargs
-            )
-        )
-
-        # get the period-to-check from the session
-        try:
-            type = self.request.session['type']
-        except:
-            self.request.session['type'] = "all"
-
-        # convert them to leaflet features
-        features = algorithms.create_features()
-        if not features:
-            features = []
-
-        context["markers"] = {
-          "type": "FeatureCollection",
-          "crs": {
-            "type": "name",
-            "properties": {
-              "name": "EPSG:4326"
-            }
-          },
-          "features": features
-        }
-
-        context["type"] = self.request.session['type']
-
-        return context
 
 class ListView(ListView):
     model = Place
     queryset = Place.objects.all()
     template_name = "list.html"
+
 
 class MapView(ListView):
     model = Place
@@ -66,7 +30,9 @@ class MapView(ListView):
         )
 
         # convert them to leaflet features
-        features = algorithms.create_features()
+        search = self.request.GET.get('ancients_search_box', None)
+        features = algorithms.create_features(search)
+
         if not features:
             features = []
 
@@ -84,4 +50,8 @@ class MapView(ListView):
         return context
 
 
+def SetPlaceFilter(request,filter):
+    request.session['places_filter'] = filter
+
+    return redirect('/ancients/?ancients_search_box=' + filter)
 
